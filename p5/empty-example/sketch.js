d0249ee641c947df85bc1dcec0b9bb3edff95308
ShaylaRose <150201746+ShaylaRose@users.noreply.github.com>
2023-12-05 19:06:36 -0500

    Simple p5Direct Link

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,159 @@
+var value = '#A3FAE0';
+
+function setup() {
+  // put setup code here
+createCanvas(1299,600);
+background('lightgreen')
+
+
+}
+
+function draw() {
+  // put drawing code here
+
+  
+  fill('#E9A520');
+  ellipse(645.5,550,730.6,40);
+
+fill(250,233,48,40);
+strokeWeight (0);
+  ellipse(650,540,770,124);
+  
+  fill('#423D15');
+  square(100.5,60,200.9,30);
+  
+  fill ('#DBDBFA');
+stroke(4)
+  strokeWeight(4)
+  square(110.5,70,180.9,30);
+
+fill('#519332');
+strokeWeight(12)
+triangle (600, 190, 900, 450, 300, 450);
+
+fill('#47822B');
+strokeWeight(7)
+triangle (600, 90, 800, 300, 400, 300);
+
+fill('#3D7025');
+noStroke();
+triangle(600, 40, 700, 150, 500, 150);
+
+fill('#A3FAE0');
+noStroke();
+ellipse (500.2,340,1.788,10);
+ 
+ ellipse (700.2,470,2.800,50);
+ ellipse (550.2,470,2.088,40);
+ ellipse (450.2,470,1.088,50);
+ ellipse (655.2,470,1.588,45);
+
+fill('#5E3014');
+stroke(51);
+stroke(color('#3D1F0D'))
+strokeWeight (4);
+rect(587, 455, 30, 100);
+  
+  fill('#A3FAE0');
+noStroke();
+  ellipse(796.2,317,1.788,38);
+  ellipse(402.2,317,1.788,38);
+
+  ellipse(796.2,463.8,1.788,38);
+  ellipse(350.2,458.8,1.788,38);
+ 
+ fill(150,0,0,180);
+  circle(530, 350, 40); 
+ 
+ fill(0,130,20,200);
+ stroke(10)
+ circle(730, 330, 30);
+ 
+ fill(100,20,250,60);
+ noStroke();
+
+ fill(87,90,200,230);
+ circle(800, 420, 40); 
+
+ fill('#244BFA');
+ circle (580,130,20);
+
+ fill('#F0E930');
+ circle (600,80,15);
+ 
+ circle(490,400,10);
+ 
+fill(255,255,255,150);
+ circle (470,460,45.6);
+
+ fill(255,255,255,190);
+ circle (670,150,27.6);
+
+ fill(255,255,255,30);
+ circle (700,300,50.6);
+
+  fill(255,255,255,250);
+ circle (184,210,77.6);
+ 
+  fill(255,255,255,250)
+ circle (185,155,50.6);
+
+ fill(255,255,255,250)
+ circle (185,115,40.6);
+
+  fill(0,0,0,250)
+ circle (176,110,8.9);
+
+ fill(0,0,0,250)
+ circle (193,110,8.9);
+
+ fill(0,0,0,250)
+ circle (184.5,117,7.5);
+
+ fill(0,0,0,250)
+ circle (184.5,126.5,2.5);
+
+fill(0,0,0,250)
+ circle (188.5,125.5,2.5);
+
+  fill(0,0,0,250)
+ circle (180.5,125.5,2.5);
+
+ fill('#CF452D')
+ circle(650,200,30);
+ 
+ fill('#FFF83B')
+ circle (500,270,20);
+   
+   fill('#A3FAE0');
+noStroke();
+ circle (590,250,30);
+
+ fill('#A3FAE0');
+noStroke();
+ ellipse (440.2,340,1.788,90);
+ ellipse (790.2,340,1.788,90);
+ ellipse (500.2,346,2.088,100);
+ ellipse (690.2,346,2.088,100);
+ ellipse (600.2,350,2.988,110);
+
+  ellipse (530.2,170,2.788,60);
+ 
+ ellipse (510.2,161,2.088,50);
+ ellipse (690.2,160,1.788,50);
+ ellipse (600.2,180,1.788,65);
+ellipse (680.2,169,1.088,50);
+}
+
+function mouseClicked (){ 
+  
+  if(value=='#A3FAE0'){ 
+    value = '#CF452D';
+  }
+  else{ 
+    value = '#A3FAE0'
+  }
+}
+
+
+  
